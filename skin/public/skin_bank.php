64fc5bdce393810d2881a7c7cbb5b173fc3e2400
Greg <ggt500@hotmail.com>
2009-07-03 11:35:21 +0100

    Replacing fieldset and legend system with nicer looking setup.

--- skin/public/skin_bank.php
+++ skin/public/skin_bank.php
@@ -19,44 +19,40 @@ class skin_bank extends skin_common {
     */
     public function make_bank($gold, $bank, $interest, $disabled, $tomorrow) {
         $bank_page = "  <h2>Bank</h2>
-                        <b>Campus Assistant:</b><br />
-                        <i>Welcome to the token bank, agent. What would you like to do?</i>
-                        <br /><br />
-                        <table width='100%'>
+                        <strong>Campus assistant:</strong>
+                        <div style='font-style:italic;'>Welcome to the token bank, agent. What would you like to do?</div>
+                        <br />
+                        <table style='width:100%;' cellspacing='4'>
                         <tr>
-                        <td width='50%'>
-                        <fieldset>
-                        <legend>Deposit tokens:</legend>
+                        <td style='width:50%;border:1px solid #CCC;padding:4px;'>
+                        <strong>Deposit tokens:</strong><br />
                         You have <b>".$gold."</b> tokens on you.<br />
                         <form method='post' action='index.php?page=bank&amp;action=deposit'>
                         <input type='text' name='amount' value='".$gold."' />
                         <input type='submit' value='Deposit'/>
                         </form>
-                        </fieldset>
                         </td>
-                        <td rowspan='2' width='50%'>
-                        <fieldset class='empty'>
-                        <legend>Collect Interest</legend>
+
+                        <td rowspan='2'  style='width:50%;border:1px solid #CCC;padding:4px;'>
+                        <strong>Collect Interest</strong><br />
                         The interest rate is at: 3%
-                        <br /><br />
+
                         <form method='post' action='index.php?page=bank&amp;action=interest'>
                         <input type='submit' value='Collect' ".$disabled." />
                         </form>
                         <br />".$tomorrow."<br />
                         Your daily interest is: <b>".$interest."</b> token points
-                        </fieldset>
                         </td>
                         </tr>
+
                         <tr>
-                        <td width='50%'>
-                        <fieldset>
-                        <legend>Withdraw tokens:</legend>
+                        <td style='width:50%;border:1px solid #CCC;padding:4px;'>
+                        <strong>Withdraw tokens:</strong><br />
                         You have <b>".$bank."</b> tokens in your bank account.<br />
                         <form method='post' action='index.php?page=bank&amp;action=withdraw'>
                         <input type='text' name='amount' value='".$bank."' />
                         <input type='submit' value='Withdraw'/>
                         </form>
-                        </fieldset>
                         </td>
                         </tr>
                         </table>";
