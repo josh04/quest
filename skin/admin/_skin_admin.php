db08cf84e66f853b2cdc63db23c0f0db8c44485d
Josh04 <josh04@gmail.com>
2009-07-14 18:42:03 +0100

    Forgot to add skin file, forgot to switch branch. Again.

---
+++ skin/admin/_skin_admin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class skin overrides for the admin section. Namely, a tiny little padlock. Aww.
+ *
+ * @author josh04
+ * @package skin_public
+ */
+class _skin_admin extends skin_common {
+    /**
+     * glues the left side to the right. yeah.
+     * @return string html
+     */
+    public function glue($section) {
+        $glue = parent::glue();
+        $glue .= "<img style='float:right;margin:8px;' src='images/icons/lock.png' alt='admin' title='Administrator page' />";
+        return $glue;
+    }
+}
+?>
