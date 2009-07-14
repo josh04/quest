84ab4502ba8a6d228cfbb39f9b5b6981e758e5ba
Greg <ggt500@hotmail.com>
2009-07-14 16:05:09 +0100

    Created help system and gave campus settings to make it more editable
    Plus, added "&act=staff" parameter to page=members to show just those with "Admin" rank

--- skin/common/functions.js
+++ skin/common/functions.js
@@ -1,14 +1,10 @@
 // Shows or hides an element, depending on its current state
-function showHide(id,icon) {
-
+function showHide(id) {
 el = document.getElementById(id);
-if(icon!=undefined) ol = document.getElementById(icon);
 if(el.style.display=="none") {
         el.style.display = "block";
-        if(ol!=undefined) ol.src = "images/dropdown_open.png";
     } else {
         el.style.display = "none";
-        if(ol!=undefined) ol.src = "images/dropdown_closed.png";
     }
 return false;
 }
