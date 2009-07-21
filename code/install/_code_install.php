f06a42dec18b9aef0468e4d58c8678f7462d692d
Josh04 <josh04@gmail.com>
2009-07-21 17:19:18 +0300

    Added some entries

--- code/install/_code_install.php
+++ code/install/_code_install.php
@@ -277,8 +277,8 @@ class _code_install extends code_common {
         (8, 'Item Blueprints', 'Admin', 'admin', 'blueprints', '', 1, 3, 0),
         (9, 'Help', 'Other', 'public', 'help', '', 1, 4, 0),
         (10, 'Support Tickets', 'Other', 'public', 'ticket', '', 1, 5, 0),
-        (11, 'Log Out', 'Other', 'public', 'login', '', 1, 6, 0);
-        (12, 'Staff List', 'Other', 'public', 'members', '&amp;action=staff', 1, 3, 0);
+        (11, 'Log Out', 'Other', 'public', 'login', '', 1, 6, 0),
+        (12, 'Staff List', 'Other', 'public', 'members', '&amp;action=staff', 1, 3, 0),
         (13, 'Menu Editor', 'Admin', 'admin', 'menu', '', 1, 4, 0);
         ";
 
